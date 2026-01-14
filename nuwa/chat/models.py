e9a3e96279db089950baa265853dd46f52821b1d
Flynotfly <Alteria2004@gmail.com>
2026-01-14 08:55:49 +0400

    Create character model

--- nuwa/chat/models.py
+++ nuwa/chat/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Character(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="characters",
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=50)
+    system_prompt = models.TextField()
+    description = models.TextField()
+    is_private = models.BooleanField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["name"])
+        ]
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return (f"<{'Public' if not self.is_private else 'Private'}"
+                f"character {self.name} of user {self.owner}>")
