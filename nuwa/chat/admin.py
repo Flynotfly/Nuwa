cfc40608d60258fa254fff4dd29706f7ca7b63fe
Flynotfly <Alteria2004@gmail.com>
2026-01-14 14:03:04 +0400

    Add character to admin

--- nuwa/chat/admin.py
+++ nuwa/chat/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Character
 
-# Register your models here.
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'is_private')
+    list_filter = ('is_private', 'owner')
+    search_fields = ('name', 'description')
+    ordering = ('name',)
