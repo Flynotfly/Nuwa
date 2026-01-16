f3a2bb2acc1233af6e81b639e2fe1a4ca84ace08
Flynotfly <Alteria2004@gmail.com>
2026-01-16 10:03:53 +0400

    [Django] Create CharacterRetrieveUpdateDestroyView

--- nuwa/chat/views/character.py
+++ nuwa/chat/views/character.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
+from django.db.models import Q
 from rest_framework.permissions import AllowAny
 
 from chat.models import Character
-from chat.serializers.character import CharacterNameSerializer
+from chat.serializers.character import CharacterNameSerializer, CharacterFullSerializer
 
 
 class CharaterListView(generics.ListAPIView):
@@ -11,3 +12,12 @@ class CharaterListView(generics.ListAPIView):
 
     def get_queryset(self):
         return Character.objects.filter(is_private=False)
+
+
+class CharacterRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CharacterFullSerializer
+
+    def get_queryset(self):
+        return Character.objects.filter(
+            Q(owner=self.request.user) | Q(is_private=False)
+        )
