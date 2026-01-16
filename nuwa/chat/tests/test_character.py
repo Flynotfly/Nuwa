443321686ce2cf02f611f220630ad26e03f21240
Flynotfly <Alteria2004@gmail.com>
2026-01-16 10:26:58 +0400

    [Django] Create character tests

--- nuwa/chat/tests/test_character.py
+++ nuwa/chat/tests/test_character.py
@@ -30,24 +30,28 @@ class CharacterTestCase(APITestCase):
             description="Description",
             system_prompt="You are bot",
             is_private=False,
+            is_hidden_prompt=False,
         )
         self.character_private = Character.objects.create(
             owner=self.user,
             name="Paul",
             system_prompt="You are bot",
             is_private=True,
+            is_hidden_prompt=False,
         )
         self.character_other_user = Character.objects.create(
             owner=self.other_user,
             name="Martin",
             system_prompt="You are bot",
             is_private=False,
+            is_hidden_prompt=False,
         )
         self.charater_private_other_user = Character.objects.create(
             owner=self.other_user,
             name="Bob",
             system_prompt="You are bot",
             is_private=True,
+            is_hidden_prompt=False,
         )
 
     def test_get_list_of_characters(self):
@@ -57,6 +61,43 @@ class CharacterTestCase(APITestCase):
         expected_ids = {self.character.pk, self.character_other_user.pk}
         self.assertEqual(returned_ids, expected_ids)
 
+    def test_get_single_character(self):
+        response = self.client.get(get_detail_url(self.character.pk))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["system_prompt"], self.character.system_prompt)
+        self.assertEqual(response.data["description"], self.character.description)
+        self.assertEqual(response.data["owner_username"], self.character.owner.username)
+
+    def test_get_character_with_hidden_prompt_owner(self):
+        char = Character.objects.create(
+            owner=self.user,
+            name="Anna",
+            description="Description",
+            system_prompt="You are bot",
+            is_private=False,
+            is_hidden_prompt=True,
+        )
+        response = self.client.get(get_detail_url(char.pk))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["system_prompt"], char.system_prompt)
+
+    def test_get_character_with_hidden_prompt_non_owner(self):
+        char = Character.objects.create(
+            owner=self.other_user,
+            name="Bob",
+            description="Description",
+            system_prompt="You are bot",
+            is_private=False,
+            is_hidden_prompt=True,
+        )
+        response = self.client.get(get_detail_url(char.pk))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["system_prompt"], "")
+
 
 def get_list_url():
     return reverse("chat:characters-list-create")
+
+
+def get_detail_url(pk: int):
+    return reverse("chat:characters-detail", kwargs={"pk": pk})
