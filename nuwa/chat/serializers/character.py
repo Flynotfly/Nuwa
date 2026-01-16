8bc97255344913a2954caf9b98236917a27f6214
Flynotfly <Alteria2004@gmail.com>
2026-01-16 09:52:36 +0400

    [Django] Create CharacterFullSerializer

--- nuwa/chat/serializers/character.py
+++ nuwa/chat/serializers/character.py
@@ -6,6 +6,26 @@ from ..models import Character
 class CharacterNameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
-        fields = ["id", "name"]
         fields = ["id", "name", "description"]
 
+
+class CharacterFullSerializer(serializers.ModelSerializer):
+    owner = serializers.CharField(source="owner.username", read_only=True)
+
+    class Meta:
+        model = Character
+        fields = [
+            "id",
+            "owner",
+            "name",
+            "description",
+            "system_prompt",
+            "is_private",
+            "is_hidden_prompt",
+        ]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        if instance.is_hidden_prompt:
+            data["system_prompt"] = ""
+        return data
