b7d6e90f0ade6007a210c0b1e4c68e0f69559884
Flynotfly <Alteria2004@gmail.com>
2026-01-21 14:33:47 +0400

    [Django] Return id on Create Chat

--- nuwa/chat/views/chat.py
+++ nuwa/chat/views/chat.py
@@ -15,8 +15,12 @@ class ChatListCreateView(generics.ListCreateAPIView):
     def get_queryset(self):
         return Chat.objects.filter(owner=self.request.user, is_active=True)
 
-    def perform_create(self, serializer):
-        serializer.save()
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        chat = serializer.save()
+        output_serializer = ChatDetailSerializer(chat)
+        return Response(output_serializer.data, status=status.HTTP_201_CREATED)
 
 
 class ChatRetrieveDestroyView(generics.RetrieveDestroyAPIView):
