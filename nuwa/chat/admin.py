8c6bc5b12744409ac91a6d50938690aef6c2f3a7
Flynotfly <Alteria2004@gmail.com>
2026-01-22 13:23:10 +0400

    [Django] Add Chat to admin panel

--- nuwa/chat/admin.py
+++ nuwa/chat/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Character
+from .models import Character, Chat
 
 
 @admin.register(Character)
@@ -9,3 +9,40 @@ class CharacterAdmin(admin.ModelAdmin):
     list_filter = ("is_private", "owner")
     search_fields = ("name", "description")
     ordering = ("name",)
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'owner',
+        'character',
+        'character_name',
+        'is_active',
+        'last_message_datetime',
+        'created_at',
+    )
+    list_filter = (
+        'is_active',
+        'is_hidden_prompt',
+        'created_at',
+        'last_message_datetime',
+    )
+    search_fields = (
+        'owner__username',
+        'character__name',
+        'character_name',
+    )
+    readonly_fields = (
+        'created_at',
+        'edited_at',
+        'last_message_text',
+        'last_message_datetime',
+    )
+    raw_id_fields = (
+        'owner',
+        'character',
+        'last_message',
+    )
+    date_hierarchy = 'last_message_datetime'
+    ordering = ('-last_message_datetime',)
