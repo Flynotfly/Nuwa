586803dadea26a830b3c34951386674bba37e8be
Flynotfly <Alteria2004@gmail.com>
2026-01-23 16:21:57 +0400

    [Django] Add rebase branch test

--- nuwa/chat/tests/test_utils.py
+++ nuwa/chat/tests/test_utils.py
@@ -113,3 +113,9 @@ class RebaseBranchTestCase(TestCase):
         history = [15, 16, 29, 30]
         result = rebase_branch(structure, history, 1)
         self.assertEqual(result, [15, 16, 29, 30, 33, 34, 35, 36])
+
+    def test_simple(self):
+        structure = [[[0], [1, [[2, [[4], [5]]], [3]]]]]
+        history = []
+        result = rebase_branch(structure, history, 1)
+        self.assertEqual(result, [1, 2, 4])
