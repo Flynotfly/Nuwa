1503b6693abf0adbf9c828057b5cb88e65056081
Flynotfly <Alteria2004@gmail.com>
2026-02-03 13:47:15 +0400

    Add local docker compose for test

---
+++ nuwa/nuwa/settings/local_test.py
@@ -0,0 +1,12 @@
+from .local import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": 5441,
+    }
+}
