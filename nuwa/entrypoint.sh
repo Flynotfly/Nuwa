07c8663346a372f11c97483ee94ba9b83fe457bc
Flynotfly <Alteria2004@gmail.com>
2026-02-11 16:05:04 +0400

    Add rebbitmq and celery to docker-compose and update entrypoint

--- nuwa/entrypoint.sh
+++ nuwa/entrypoint.sh
@@ -3,13 +3,13 @@ set -e
 
 ./wait-for-it.sh db:5432 -t 30
 
-mkdir -p /run/uwsgi
-chown www-data:www-data /run/uwsgi
-
-echo "Running database migrations..."
-uv run --no-sync python manage.py migrate --noinput
-
-echo "Running collect static..."
-uv run --no-sync python manage.py collectstatic --noinput
+if ["$ROLE" = "web"]; then
+	mkdir -p /run/uwsgi
+	chown www-data:www-data /run/uwsgi
+	echo "Running database migrations..."
+	uv run --no-sync python manage.py migrate --noinput
+	echo "Running collect static..."
+	uv run --no-sync python manage.py collectstatic --noinput
+fi
 
 exec "$@"
