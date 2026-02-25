795930cdb78d294943d90ea612cdf4caf5be9257
Flynotfly <Alteria2004@gmail.com>
2026-02-25 14:03:42 +0400

    [Django] Include routers

--- nuwa_fastapi/src/router.py
+++ nuwa_fastapi/src/router.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 
 from src.views.user import user_router
+from src.views.character import character_router
+from src.views.chat import chat_router
 
 router = APIRouter()
 router.include_router(user_router)
+router.include_router(character_router)
+router.include_router(chat_router)
