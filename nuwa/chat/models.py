afde6207988c32ec31af1dd03f6c2b273d3cc731
Flynotfly <Alteria2004@gmail.com>
2026-02-25 14:15:34 +0400

    [Django] Return last_message_datetime to blank

--- nuwa/chat/models.py
+++ nuwa/chat/models.py
@@ -66,7 +66,7 @@ class Chat(models.Model):
         null=True,
     )
     last_message_text = models.TextField(blank=True, null=True)
-    last_message_datetime = models.DateTimeField()
+    last_message_datetime = models.DateTimeField(blank=True, name=True)
     structure = models.JSONField(default=list)
 
     created_at = models.DateTimeField(auto_now_add=True)
